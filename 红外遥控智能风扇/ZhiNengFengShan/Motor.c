c6ad7a088d76bc3be219481dfe642352cfc5ccbd
Qingcheng Liu <69419274+Youyubuyu@users.noreply.github.com>
2024-01-31 23:50:45 +0800

    Add files via upload

---
+++ 红外遥控智能风扇/ZhiNengFengShan/Motor.c
@@ -0,0 +1,32 @@
+#include <REGX52.H>
+#include "Timer1.h"
+
+sbit Motor = P1^0;
+
+unsigned char Counter, Compare;
+
+void Motor_Init()
+{
+	Timer1_Init();
+}
+
+void Motor_SetSpeed(unsigned char Speed)
+{
+	Compare = Speed;
+}
+
+void Timer1_Routine() interrupt 3
+{
+	TL1 = 0x9C;				//设置定时初值
+	TH1 = 0xFF;				//设置定时初值
+	Counter++;
+	Counter %= 100;
+	if(Counter < Compare)
+	{
+		Motor = 1;
+	}
+	else
+	{
+		Motor = 0;
+	}
+}
